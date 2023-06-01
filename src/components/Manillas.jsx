929051de607ae263448422ec78200a8a7c7a2b1e
Bladimiro Ricaurte <bladimirolcd@gmal.com>
2023-05-31 19:36:32 -0500

    Inclusión configuración Firebase

--- src/components/Manillas.jsx
+++ src/components/Manillas.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
+import { collection, doc, addDoc, onSnapshot, deleteDoc, updateDoc } from 'firebase/firestore'
 
 const Manillas = () => {
+    const [material, setMaterial] = useState('')
+    const [dije, setDije] = useState('')
+    const [tipo, setTipo] = useState('')
+    const [valor, setValor] = useState('')
+    const [listaManillas, setListaManillas] = useState([])
+
+    useEffect(()=>{
+        const obtenerDatos = async() =>{
+            try{
+                await onSnapshot(collection(db, 'manillas'), (query) =>{
+                    setListaLibros(query.docs.map((doc)=>({...doc.data(), id:doc.id})))
+                })
+            }catch(error){
+                console.log(error)
+            }
+        }
+        obtenerDatos();
+    }, [])
   return (
-    <div>Manillas</div>
+    <div>
+        <h1>XYZ - Manillas</h1>
+    </div>
   )
 }
 
